bdfa3a862b2f911760621babafbc6dbb04694457
moraditoo <damora@uamv.edu.ni>
2026-03-13 15:30:08 -0600

    cambios

---
+++ src/main/VariablePrimitiva.java
@@ -0,0 +1,23 @@
+package main;
+
+public class VariablePrimitiva
+{
+    public static void main(String[] args)
+    {
+        //Primitivos
+        int numero = 10;
+        System.out.println("El numero es: " + numero);
+
+        float capital = 1999999.99f;
+        System.out.println("El capital es: "+ capital);
+
+        double decimal = 1999999.99;
+        System.out.println("El decimal es: " + decimal);
+
+        boolean estado = true;
+        System.out.println("El estado es: " + estado);
+
+        String nombre = "Dylan";
+        System.out.println("El nombre es" + nombre);
+    }
+}
